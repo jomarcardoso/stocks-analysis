5813dc5e1a05b79096450747e3bea7cf3533eb3e
jomarcardoso <antonionetocardoso@gmail.com>
2023-12-31 01:35:25 -0300

    web components

--- public/scripts/dom.mjs
+++ public/scripts/dom.mjs
@@ -1,16 +1,35 @@
 import { EXECUTE_SCRIPTS } from './extension.mjs';
 
-export async function getLogo(browser, website) {
-  console.log(browser);
-
-  const oi = await EXECUTE_SCRIPTS[browser](() => {
+export async function getLogoUrl(browser, website) {
+  const logoUrl = await EXECUTE_SCRIPTS[browser](() => {
     var logoEl = document.querySelector('[title^="Logotipo da empresa"]');
 
     return logoEl
       .getAttribute('data-img')
       .replace('url(', 'https://statusinvest.com.br')
-      .replace(/\)$/, '');
+      .replace(/\)$/, '')
+      .replace('/cover/', '/avatar/');
   });
 
-  document.body.insertAdjacentHTML('afterbegin', `<img src="${oi}">`);
+  return logoUrl;
+}
+
+export async function getTicker(browser, website) {
+  const ticker = await EXECUTE_SCRIPTS[browser](() => {
+    var ticker = document.querySelector('a[title^="Detalhes do ticker"]');
+
+    return ticker.textContent;
+  });
+
+  return ticker;
+}
+
+export async function getName(browser, website) {
+  const name = await EXECUTE_SCRIPTS[browser](() => {
+    var name = document.querySelector('h1 small');
+
+    return name.textContent;
+  });
+
+  return name;
 }
--- public/scripts/index.mjs
+++ public/scripts/index.mjs
@@ -1,16 +1,46 @@
 import { getBrowser } from './browser.mjs';
-import { getLogo } from './dom.mjs';
+import { getLogoUrl, getName, getTicker } from './dom.mjs';
 import { getWebsite } from './website.mjs';
 
 async function start() {
   const browser = getBrowser();
   const website = await getWebsite(browser);
+  const logo = await getLogoUrl(browser);
+  const ticker = await getTicker(browser);
+  const name = await getName(browser);
 
-  getLogo(browser);
+  class Logo extends HTMLImageElement {
+    connectedCallback() {
+      this.setAttribute('src', logo);
+      this.classList.add('border-2', 'border-gray-400');
+    }
+  }
 
-  // configPopup();
+  customElements.define('sa-logo', Logo, { extends: 'img' });
 
-  console.log(browser, website);
+  class Ticker extends HTMLHeadingElement {
+    connectedCallback() {
+      this.innerHTML = ticker;
+      this.classList.add(
+        'font-mono',
+        'text-3xl',
+        'font-bold',
+        'leading-none',
+        'whitespace-nowrap',
+      );
+    }
+  }
+
+  customElements.define('sa-ticker', Ticker, { extends: 'h1' });
+
+  class Name extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = name;
+      this.classList.add('font-sans', 'text-lg');
+    }
+  }
+
+  customElements.define('sa-name', Name);
 }
 
 start();
